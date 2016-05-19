f76157115bbde18eb44263e8976f807d186ff673
bibi21000 <bibi21000@gmail.com>
2016-05-19 18:32:52 +0200

    Update doc

--- src/janitoo_voxgenerator/thread_voxgenerator.py
+++ src/janitoo_voxgenerator/thread_voxgenerator.py
@@ -50,7 +50,7 @@ assert(COMMAND_DESC[COMMAND_CONTROLLER] == 'COMMAND_CONTROLLER')
 OID = 'voxgen'
 
 def make_thread(options, force=False):
-    if get_option_autostart(options, OID) == True or force:
+    if get_option_autostart(options, OID) or force:
         return VoxgenThread(options)
     else:
         return None
